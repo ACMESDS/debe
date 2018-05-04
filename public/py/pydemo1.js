d300307fdad627a4ee02a2e3898dfa299ac9a57c
acmesds@somemail.com <acmesds@nowhere.org>
2018-05-04 14:08:52 -0500

    fix serv init

--- public/py/pydemo1.js
+++ public/py/pydemo1.js
@@ -13,15 +13,15 @@ the end. `
 	},
 
 	engine: `
-def pydemo1(ctx,res):
-print "welcome to python you lazy bird"
-#print OS
-ctx['Save'] = [ {'x':1, 'y':2, 'z':0}, {'x':3, 'y':4, 'z':10}]
-res(ctx)
-SQL0 = OS['SQL0']
-SQL0.execute("SELECT * from app.Htest", () )
-#SQL0.execute("SELECT 1 as x, 2 as y", () )
-for (Rec) in SQL0:
-	print Rec
+def pydemo1(ctx):
+	print "welcome to python you lazy bird"
+	#print OS
+	SQL0 = OS['SQL0']
+	SQL0.execute("SELECT * from app.Htest", () )
+	#SQL0.execute("SELECT 1 as x, 2 as y", () )
+	for (Rec) in SQL0:
+		print Rec
+	
+	return [ {'x':1, 'y':2, 'z':0}, {'x':3, 'y':4, 'z':10}]
 `
 };
