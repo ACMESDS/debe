8e0eb2b156529b188391d2fee2e576c564e5aafc
acmesds@somemail.com <acmesds@nowhere.org>
2018-08-02 15:51:24 -0500

    add date weeknum

--- debe.js
+++ debe.js
@@ -3547,6 +3547,18 @@ Totem(req,res) endpoint to send emergency message to all clients then halt totem
 ].extend(Array);
 	
 [  // date prototypes
+	function getWeek() {
+		  var date = new Date(this.getTime());
+		   date.setHours(0, 0, 0, 0);
+		  // Thursday in current week decides the year.
+		  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+		  // January 4 is always in week 1.
+		  var week1 = new Date(date.getFullYear(), 0, 4);
+		  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+		  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+								- 3 + (week1.getDay() + 6) % 7) / 7);
+	},
+	
 	function addDays(days) {
 		var d = new Date(this);
 		d.setDate( d.getDate() + days);
