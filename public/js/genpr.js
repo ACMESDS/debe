b6b3f8c91ce62371dbd879eb9f1485beb2450811
acmesds@somemail.com <acmesds@nowhere.org>
2018-11-05 17:26:43 -0600

    refactor and integrate cints into genpr

--- public/js/genpr.js
+++ public/js/genpr.js
@@ -1,10 +1,15 @@
 module.exports = {  // generate a Markov process given its transition probability parameters
 	keys: {
 		emProbs: "json comment 'gaussian mixing parameters { dims: [D1, D2 , ...] weights: [W1, W2 , ...] }' ",
-		trProbs: "json comment 'KxK transition probs [ [pr, pr, ...], ...] or {fr: {to: pr, ... }, ... K: states} ' ",
 		Symbols: "json comment '[S1, S2, ... ] state symbols or null to generate defaults' ",
 		Members: "int(11) default 100 comment 'number in process ensemble' ",
-		Wiener: "int(11) default 0 comment 'number of wiener processes; 0 disables' ",
+		
+		type_Markov: "json comment 'Markov process with KxK transition probs [ [pr, ...], ...] || {states: K, fr: {to: pr, ... } , ... \"fr,...\": \"to,...\" }' "	,
+		type_Wiener: "int(11) default 0 comment 'Wiener process with specified diffusion walks' ",
+		type_Bayes: "json comment 'Bayes process with specified equilibrium probs [pr, ... ]' ",
+		type_Gauss: "json comment 'Gauss proccess with specified {ints: coherence intervals, mean: mean count, maxints: max ints, lim: mineig, model: sinc||rect, }' ",
+		type_Gillespie: "int(11) comment 'Gillespie process with specified number of states' ",
+		
 		Nyquist: "float default 1 comment 'process over-sampling factor' ",
 		Steps: "int(11) default 0 comment 'number of process steps' ",
 		Batch: "int(11) default 0 comment 'steps to next supervised learning' ",
@@ -33,6 +38,253 @@ module.exports = {  // generate a Markov process given its transition probabilit
 			return x;
 		}
 
+		function KL( solve, cb ) { // Karhouen-Loeve expansion with callback cb(pcs) || cb(null)
+			
+			function getpcs(model, Emin, M, Mwin, Mmax, cb) {  // callback cb(pcs) || cb(null)
+
+				function genpcs(dim, steps, model, cb) {  // callback callback cb(pcs = {values,vectors,ref})
+					Log("gen pcs", {Mmax: dim, Msteps: steps, model: model}); 
+					
+					function evd( models, dim, step, cb) {   // eigen value decomp with callback cb(pcs)
+						models.forEach( function (model) {
+
+							Log("pcs", model, dim, step);
+
+							for (var M=1; M<dim; M+=step) {
+								var 
+									ctx = {
+										N: dim,
+										M: M,
+										T: 50
+									},
+									script = `
+t = rng(-T, T, 2*N-1);
+Tc = T/M;
+xccf = ${model}( t/Tc );
+Xccf = xmatrix( xccf ); 
+R = evd(Xccf); 
+`; 
+
+								ME.exec( script,  ctx, function (ctx) {
+
+									if (solve.trace)  // debugging
+										ME.exec(`
+disp({
+M: M,
+ccfsym: sum(Xccf-Xccf'),
+det: [det(Xccf), prod(R.values)],
+trace: [trace(Xccf), sum(R.values)]
+})`, ctx);
+
+/*
+basis: R.vectors' * R.vectors,
+vecres: R.vectors*diag(R.values) - Xccf*R.vectors,
+*/
+									cb({
+										model: model,
+										intervals: M,
+										values: ctx.R.values._data,
+										vectors: ctx.R.vectors._data
+									});
+								});
+							}
+						});
+					}
+
+					evd( [model], Mmax, Mwin*2, function (pcs) {
+
+						var 
+							vals = pcs.values,
+							vecs = pcs.vectors,
+							N = vals.length, 
+							ref = pcs.ref = ME.max(vals);
+
+						Log("evd vals=", vals.length );
+						cb( pcs );  // forward them on then save em
+						
+						SQL.beginBulk();
+
+						vals.forEach( (val, idx) => {
+							var
+								save = {
+									correlation_model: pc.model,
+									coherence_intervals: pc.intervals,
+									eigen_value: val / ref,
+									eigen_index: idx,
+									ref_value: ref,
+									max_intervals: dim,
+									eigen_vector: JSON.stringify( vecs[idx] )
+								};
+
+							//Log(save);
+							
+							SQL.query("INSERT INTO app.pcs SET ? ON DUPLICATE KEY UPDATE ?", [save,save] );
+						});
+						
+						SQL.endBulk();
+						Log("evd saved pcs");
+						SQL.release();
+					});
+				}
+
+				function findpcs( cb ) {		// callback cb(pcs = {values,vectors,ref})
+					var M0 = Math.min( M, Mmax-Mwin*2 );
+
+					var q = SQL.query(
+						"SELECT * FROM app.pcs WHERE coherence_intervals BETWEEN ? AND ? AND eigen_value / ref_value > ? AND least(?,1) ORDER BY eigen_index", 
+						[M0-Mwin, M0+Mwin, Emin, {
+							max_intervals: Mmax, 
+							correlation_model: model
+						}],
+						function (err, pcs) {
+
+							if ( err ) 
+								Log( TRACE, err );
+
+							else {
+								var vals = [], vecs = [];
+
+								pcs.forEach( function (pc) {
+									vals.push( pc.eigen_value );
+									vecs.push( JSON.parse( pc.eigen_vector ) );
+								});
+
+								cb({
+									values: vals,
+									vectors: vecs,
+									ref: pcs.length ? pcs[0].ref_value : 0
+								});
+								SQL.release();
+							}
+					});
+
+					Log(q.sql);
+				}
+
+				findpcs( function (pcs) {
+					if ( pcs.values.length )   // found pcs so send them on
+						cb( pcs );
+					
+					else  // try to generate pcs
+						genpcs( Mmax, Mwin*2, model, (pcs) => {
+							Log("gen pcs", pcs.values.length);
+							if ( pcs.values.length )
+								cb( pcs );
+							else
+								cb( null );
+						});
+						/*
+						SQL.query(
+							"SELECT count(ID) as Count FROM app.pcs WHERE least(?,1)", {
+								max_intervals: Mmax, 
+								correlation_model: model
+							}, 
+							function (err, test) {  // see if pc model exists
+
+							//Log("test", test);
+							if ( !test[0].Count )  // pc model does not exist so make it
+								genpcs( Mmax, Mwin*2, model, function () {
+									findpcs( sendpcs );
+								});
+
+							else  // search was too restrictive so no need to remake model
+								sendpcs(pcs);
+								
+						});*/
+				});
+			}
+	
+			// Should add a ctx.Shortcut parms to bypass pcs and use an erfc model for eigenvalues.
+
+			//Log(solve);
+			if ( solve.model )
+				getpcs( solve.model, solve.min, solve.M, solve.Mstep/2, solve.Mmax, cb );
+			
+			else
+				cb( null );
+				
+				/*
+				const { sqrt, random, log, exp, cos, sin, PI } = Math;
+				
+				function expdev(mean) {
+					return -mean * log(random());
+				}
+				
+				if (pcs) {
+					var 
+						pcRef = pcs.ref,  // [unitless]
+						pcVals = pcs.values,  // [unitless]
+						N = pcVals.length,
+						T = solve.T,
+						dt = T / (N-1),
+						egVals = $(N, (n,e) => e[n] = solve.lambdaBar * dt * pcVals[n] * pcRef ),  // [unitless]
+						egVecs = pcs.vectors,   // [sqrt Hz]
+						ctx = {
+							T: T,
+							N: N,
+							dt: dt,
+							
+							E: ME.matrix( egVals ),
+							
+							B: $(N, (n,B) => {
+								var
+									b = sqrt( expdev( egVals[n] ) ),  // [unitless]
+									arg = random() * PI;
+
+								Log(n,arg,b, egVals[n], T, N, solve.lambdaBar );
+								B[n] = ME.complex( b * cos(arg), b * sin(arg) );  // [unitless]
+							}),
+
+							V: egVecs   // [sqrt Hz]
+						},
+						script = `
+A=B*V; 
+lambda = abs(A).^2 / dt; 
+Wbar = {evd: sum(E), prof: sum(lambda)*dt};
+evRate = {evd: Wbar.evd/T, prof: Wbar.prof/T};
+x = rng(-1/2, 1/2, N); 
+`;
+
+//Log(ctx);
+
+					if (N) 
+						ME.exec( script , ctx, (ctx) => {
+							//Log("ctx", ctx);
+							cb({
+								intensity: {x: ctx.x, i: ctx.lambda},
+								//mean_count: ctx.Wbar.evd,
+								//mean_intensity: ctx.evRate.evd,
+								eigen_ref: pcRef
+							});
+							Log({
+								mean_count: ctx.Wbar,
+								mean_intensity: ctx.evRate,
+								eigen_ref: pcRef
+							});
+						});	
+
+					else
+						cb({
+							error: `coherence intervals ${stats.coherence_intervals} > max pc dim`
+						});
+				}
+				
+				else
+					cb({
+						error: "no pcs matched"
+					});
+*/
+		}
+		
+		function genProc(opts, res) {
+			var ran = new RAN(opts);  // create a random process compute thread
+
+			ran.pipe( function (evs) {  // sync the process events to this callback
+				ctx.Save = evs;
+				res( ctx );
+			});   // run process and capture results
+		}
+		
 		const {exp,log,sqrt,floor,rand} = Math;
 		/*
 		var 
@@ -99,11 +351,15 @@ module.exports = {  // generate a Markov process given its transition probabilit
 			sampler = samplers[mode], // sampler
 			*/
 
-		var ran = new RAN({ // generating supervisor
+		var opts = { // supervisor config 
 			N: ctx.Members,  // ensemble size
-			wiener: ctx.Wiener,  // wiener process steps
-			trP: ctx.trProbs, // state transition probs 
 			symbols: ctx.Symbols,  // state symbols
+			
+			wiener: ctx.type_Wiener, // walks
+			markov: ctx.type_Markov, // trans probs
+			gauss: ctx.type_Gauss, // [mean count, coherence intervals]
+			bayes: ctx.type_Bayes, // equlib probs
+			
 			dt: 1/ctx.Nyquist, // oversampling factor
 			steps: ctx.Steps, // process steps
 			emP: ctx.emProbs,  	// mixing/emission/observation parms
@@ -172,12 +428,55 @@ module.exports = {  // generate a Markov process given its transition probabilit
 						//str.push(ev);
 				}			
 			}  // event saver 
-		});  // create a randpr compute thread
+		};
 
-		ran.pipe( function (evs) {  // sync pipe
-			ctx.Save = evs;
-			res( ctx );
-		});   // run process and capture results
+		if (gparms = opts.gauss) {
+			
+			var 
+				T = ctx.Steps,
+				dt = 1/ctx.Nyquist;
+			
+			if (gparms.model && gparms.ints)
+				KL({  // parms for Karhunen Loeve solver
+					trace: false,   // eigen debug
+					T: T,  // observation interval  [1/Hz]
+					M: gparms.ints , // coherence intervals
+					lambdaBar: gparms.mean * dt/T, // event arrival rate [Hz]
+					Mstep: 1,  // coherence step size when pcs are generated
+					Mmax: gparms.maxints || 150,  // max coherence intervals when pcs are generated
+					model: gparms.model || "sinc",  // assumed correlation model for underlying CCGP
+					min: gparms.lim || 0	// min eigen value to use
+				}, function (pcs) {
+
+					if (pcs) {  // use eigen expansion to gen gauss states/deviates
+						
+						Log("KL pcs", pcs.values.length, opts);
+						
+						var
+							vals = pcs.values,
+							vecs = pcs.vectors;
+						
+						opts.gauss = function (t) {
+							Log("gauss t", t);
+						};
+						
+						genProc(opts, res);
+					}
+
+					else 
+						res( null );
+				});
+			
+			else { // use negbin to gen bayes states
+				Log("mh/mcmc tbd");
+				opts.bayes = gparms.ints ? [] : []; // negbin || poisson
+				genProc(opts, res);
+			}
+			
+		}
+		
+		else
+			genProc(opts, res);
 		
 	}
 
