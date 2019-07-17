e63573078cef29ca1e5013f4feee29e516670cac
acmesds@somemail.com <acmesds@nowhere.org>
2019-07-17 17:12:30 -0500

    chipping fixes

--- public/js/aois.js
+++ public/js/aois.js
@@ -1,19 +1,20 @@
 module.exports = {
 	keys: {
+		Name: "varchar(64)",
 		chipPixels: "int(11)",
 		chipFeatures: "float",
 		featureLength: "float",
 		ring: "json",
 		featureOverlap: "float",
 		radius: "float",
+		voxelCount: "int(11)",
+		voxelHeight: "float",
 		Description: "mediumtext"
 	},
 	
 	engine: function aois(ctx,res) {
 		res(ctx);
-		$.thread( sql => {
-			HACK.voxelizeAOI(sql, ctx);
-		});
+		$.voxelize(ctx);
 	}	
 
 }
