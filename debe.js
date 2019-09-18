bf99ae514a9598cd21b50e26980c2cbb43d6811b
acmesds@somemail.com <acmesds@nowhere.org>
2019-09-17 19:05:44 -0500

    refactor i/f

--- debe.js
+++ debe.js
@@ -61,6 +61,7 @@ var
 	LANG = require('i18n-abide'), 		//< I18 language translator
 	ARGP = require('optimist'),			//< Command line argument processor
 	TOKML = require("tokml"), 			//< geojson to kml convertor
+	JAX = require("mathjax-node"),   //< servde side mathjax parser
 	
 	// totem modules		
 	EAT = require("./ingesters"),	
@@ -73,6 +74,10 @@ var
 const { Copy,Each,Log,isObject,isString,isFunction,isError,isArray } = ENUM;
 const { sqlThread, getFile, getSite, thread } = TOTEM;
 const { getVoxels } = GEO;
+
+function Trace(msg,sql) {
+	"blog>".trace(msg,sql);
+}
 
 var
 	DEBE = module.exports = TOTEM;
@@ -469,7 +474,16 @@ catch (err) {
 		initSES( function () {	// init session handelling
 		initSQL( function () {	// init the sql interface
 
-			FLEX({ 
+			JAX.config({		// for blogging
+				MathJax: {
+					tex2jax: {
+						//displayMath: [["$$","$$"]]
+					}
+				}
+			});
+			JAX.start();
+			
+			FLEX({ 		// table emulation
 				thread: sqlThread,
 				//emitter: DEBE.IO ? DEBE.IO.sockets.emit : null,
 				//skinner: JADE,
@@ -480,20 +494,20 @@ catch (err) {
 				site: DEBE.site						// Site parameters
 			});
 
-			GEO.config({
+			GEO.config({	// voxelizing geo surfaces
 				//source: "",
 				taskPlugin: null,
 				thread: DEBE.thread,
 				getSite: DEBE.getSite
 			});
 
-			$.config({
+			$.config({		// matrix manipulator
 				thread: sqlThread,
 				runTask: DEBE.runTask,
 				getSite: DEBE.getSite
 			});
 
-			ATOM.config({
+			ATOM.config({		// plugin manager
 				thread: sqlThread,
 				cores: DEBE.cores,
 				//watchFile: DEBE.watchFile,
@@ -514,30 +528,28 @@ catch (err) {
 
 			DEBE.onStartup();
 
-			var path = DEBE.paths.jades;
+			if ( path = DEBE.paths.jades )
+				DEBE.getIndex( path, (files) => {  // publish new engines
+					var ignore = {".": true, "_": true};
+					files.forEach( (file) => {
+						if ( !ignore[file.charAt(0)] )
+							try {
+								Trace("PUBLISHING "+file);
 
-			if (false)
-			DEBE.getIndex( path, (files) => {  // publish new engines
-				var ignore = {".": true, "_": true};
-				files.forEach( (file) => {
-					if ( !ignore[file.charAt(0)] )
-						try {
-							Trace("PUBLISHING "+file);
+								sql.query( "REPLACE INTO app.engines SET ?", {
+									Name: file.replace(".jade",""),
+									Code: FS.readFileSync( path+file, "utf-8"),
+									Type: "jade",
+									Enabled: 0
+								});
+							}
+							catch (err) {
+								//Trace(err);
+							}
+					});
 
-							sql.query( "REPLACE INTO app.engines SET ?", {
-								Name: file.replace(".jade",""),
-								Code: FS.readFileSync( path+file, "utf-8"),
-								Type: "jade",
-								Enabled: 0
-							});
-						}
-						catch (err) {
-							//Trace(err);
-						}
+					sql.release();
 				});
-
-				sql.release();
-			});
 
 		}); }); }); 
 	},
@@ -1989,7 +2001,8 @@ Trace(`NAVIGATE Recs=${recs.length} Parent=${Parent} Nodes=${Nodes} Folder=${Fol
 		gohome: "Totem".tag("/fan.view?src=info&w=1000&h=600")  + ": protecting the warfighter from bad data",
 		
 		engine: "SELECT * FROM app.engines WHERE least(?,1) LIMIT 1",
-		jades: "./public/jade/",		// path to default view skins
+		jades: "", // "./public/jade/",		// path to default view skins
+		jadePath: "./public/jade/ref.jade",	// jade reference path for includes, exports, appends
 		
 		mime: {
 			/*
@@ -4537,6 +4550,441 @@ Totem (req,res)-endpoint to render req.table using its associated jade engine.
 		});
 	});
 }
+
+/**
+@class DEBE.blogs
+*/
+
+[  // string prototypes
+	
+	// string serializers 
+	function Xblog(req, ds, cache, ctx, rec, viaBrowser, cb) {
+	/**
+	@member String
+	Expands markdown of the form:
+		
+		[ TEXT ] ( PATH.TYPE ? w=WIDTH & h=HEIGHT & x=KEY$INDEX & y=KEY$INDEX ... )  
+		[ TEXT ] ( COLOR )  
+		[ TOPIC ] ( ? starts=DATE & ends=DATE )  
+		$$ inline TeX $$  ||  n$$ break TeX $$ || a$$ AsciiMath $$ || m$$ MathML $$  
+		[JS || #JS || TeX] OP= [JS || #JS || TeX]  
+		$ { KEY } || $ { JS } || $ {doc( JS , "INDEX" )}  
+		KEY,X,Y >= SKIN,WIDTH,HEIGHT,OPTS  
+		KEY <= VALUE || OP <= EXPR(lhs),EXPR(rhs)  
+		
+	using the supplifed cache to get/put KEY values, as well as block-escaping:
+				
+		HEADER:
+
+			BLOCK
+
+	and markdown scripting:
+
+		MARKDOWN
+		script:
+		MATLAB EMULATION SCRIPT
+		
+	@param {Object} req Totem request
+	@param {String} ds default dataset in [post](URL) markdown
+	@param {Object} cache hash for cacheing markdown variables
+	@param {Object} ctx hash holding markdown variables
+	@param {Obect} rec record hash for markdown variables
+	@param {Boolean} viaBrowser true to render markdown to browser
+	@param {Function} cb callback(markdown html)
+	*/
+		
+		for (var key in rec)  { // parse json stores
+			try { 
+				ctx[key] = JSON.parse( rec[key] ); 
+			} 
+			catch (err) { 
+				ctx[key] = rec[key]; 
+			}
+		}
+	
+		var 
+			blockidx = 0;
+		
+		Copy(blogContext, ctx);
+		
+		this.Xescape( [], (blocks,html) => // escape code blocks
+		html.Xbreaks( html => // force new lines
+		html.Xsection( html => // expand section headers
+		html.Xscript( ctx, (ctx,html) => // expand scripts 
+		html.Xkeys( ctx, html => // expand js keys
+		html.Xgen(ctx, html => // expand generators
+		html.Xtex( html => // expand TeX
+		html.Xlink( req, ds, viaBrowser, html => { // expand links
+
+			if ( viaBrowser )
+				html = 	html.replace(/href=(.*?)\>/g, (str,ref) => { // smart links to follow <a href=REF>A</a> links
+					var q = (ref.startsWith( "'" )) ? '"' : "'";
+					return `href=${q}javascript:navigator.follow(${ref},BASE.user.client,BASE.user.source)${q}>`;
+				});
+
+			cb( html.replace(/@block/g, str => {  // backsub escaped blocks
+					//Log(`unblock[${blockidx}]`, blocks[ blockidx].tag("code",{}) );
+					return blocks[ blockidx++ ].tag("code",{}).tag("pre",{});
+				}) );
+			
+		}))))))));
+	},
+	
+	function Xbreaks( cb ) {
+		cb( this.replace( /  \n/g, "<br>" ) );
+	},
+	
+	function Xescape( blocks, cb ) { // escapes code blocks then callsback cb(blocks, html)
+		var 
+			key = "@esc",
+			html = this,
+			fetchBlock = function ( rec, cb ) {	// callsback cb with block placeholder
+				//Log(`block[${blocks.length}] `, rec.arg1 );
+				blocks.push( rec.arg2 );
+				cb( rec.arg1 + ":" + "@block");
+			},
+			pattern = /(.*)\:\n\n((\t.*\n)+)\n/gm ;
+		
+		html.serialize( fetchBlock, pattern, key, (html, fails) => {  
+			cb( blocks, html);
+		}); 		
+	},
+	
+	function Xdummy(cb) {  // for debugging with callback(this)
+		cb(this);
+	},
+	
+	function Xlink( req, ds, viaBrowser, cb ) {  // expands [LINK](URL) tags then callsback cb( final html )
+		/*
+		req = http request or null to disable smart hash tags (content tracking)
+		ds = dataset?query default url path
+		viaBrowser = true to enable produce html compatible with browser
+		*/
+		var 
+			key = "@tag",
+			html = this,
+			//getSite = TOTEM.getSite,
+			fetchTrack = function ( rec, cb) {  // callback cb with expanded [TOPIC]() markdown
+				var 
+					secret = "",
+					topic = rec.topic,
+					product = topic+".html";
+
+				if (req)		// content tracking enabled
+					if ( licenseCode = FLEX.licenseCode )
+						licenseCode( req.sql, html, {  // register this html with the client
+							_Partner: req.client,
+							_EndService: "",  // leave empty so lincersor wont validate by connecting to service
+							_Published: new Date(),
+							_Product: product,
+							Path: "/tag/"+product
+						}, (pub, sql) => {
+							if (pub) {
+								cb( `${rec.topic}=>${req.client}`.tag( "/tags.view" ) );
+								sql.query("INSERT INTO app.tags SET ? ON DUPLICATE KEY UPDATE Views=Views+1", {
+									Viewed: pub._Published,
+									Target: pub._Partner,
+									Topic: topic,
+									License: pub._License,
+									Message: "get view".tag( "/decode.html".tag("?",{Target:pub._Partner,License:pub._License,Topic:topic}))
+								});
+							}
+						});
+				
+					else
+						Trace( "NO CODE LICENSOR" );
+				
+				else	// content tracking disabled
+					cb( "" );
+			},
+			
+			fetchSite = function ( rec, cb ) {  // callback cb with expanded [](URL) markdown
+				//Log("solicit", rec, viaBrowser);
+				if (viaBrowser) 
+					cb( "".tag("iframe", {src:rec.arg3}) );
+				
+				else
+					getSite( rec.arg3, null, cb );
+			},
+			
+			fetchLink = function ( rec, cb ) {  // expand [LINK](URL) markdown
+				
+				var
+					op = rec.arg1,
+					opt = rec.arg2,
+					url = rec.arg3;
+
+				if (op) 	// OP [ LABEL] (URL )
+					cb( `${op}[${opt}](${url})` );
+				
+				else
+				if (opt)	//	[ LABEL ] ( URL )
+					cb( opt.tag( url ) );
+
+				else {	// [ ] ( URL )
+					var
+						colors = {
+							r: "red", 
+							b: "blue",
+							g: "green",
+							y: "yellow",
+							o: "orange",
+							k: "black",
+							red: "red",
+							blue: "blue",
+							green: "green",
+							yellow: "yellow",
+							orange: "orange",
+							black: "black"
+						},						
+						keys = {},
+						dsPath = ds.parseURL(keys,{},{},{}),
+						urlPath = url.parseURL(keys,{},{},{}),
+						
+						w = keys.w || 200,
+						h = keys.h || 200,
+						
+						now = new Date(),
+
+						x = urlPath.replace(/(.*)\.(.*)/, (str,L,R) => {
+							urlName = L;
+							urlType = R;
+							return "#";
+						}),
+						
+						srcPath = urlPath.tag( "?", Copy({src:dsPath}, keys) );
+
+					
+					// Log("link", [dsPath, srcPath, urlPath], keys, [opt, url]);
+
+					switch (urlType) {  //  [](PATH.TYPE?w=W&h=H)
+						case "jpg":  
+						case "png":
+							cb( "".tag("img", { src:`${url}?killcache=${new Date()}`, width:w, height:h }) );
+							break;
+
+						case "view": 
+							if ( viaBrowser ) 
+								cb( "".tag("iframe", { src: srcPath, width:w, height:h }) );
+							
+							else
+								cb( urlPath.tag( url ) );
+							
+							break;
+
+						default: 
+							if ( viaBrowser )
+								cb( "".tag("iframe", { src:srcPath, width:w, height:h }) );
+
+							else
+								fetchSite(rec, cb);
+
+							/*
+							if ( color = colors[urlPath.toLowerCase()] )		// [ TEXT ]( COLOR )
+								cb( opt.tag("font",{color:color}) );
+							*/
+							/*
+							else		// [ TEXT ]( URL )
+							if ( (keys.starts ? now>=new Date(keys.starts) : true) && 
+								 (keys.ends ? now<=new Date(keys.ends) : true) ) {
+								rec.topic = opt;
+								fetchTrack( rec, cb );
+							}
+
+							else
+								cb( opt.tag( "/tags.view" ) );
+							*/
+							
+							break;
+					}
+				}
+			},
+			
+			pattern = /(\!?)\[([^\[\]]*)\]\(([^\)]*)\)/g ;
+		
+		html.serialize( fetchLink, pattern, key, html => {    
+			cb(html);
+		}); 
+	},
+	
+	function Xscript( ctx, cb ) {  // expands scripting tags then callsback cb(vmctx, final markdown)
+		var 
+			script = "",
+			pattern = /script:\n((.|\n)*)/g,  // defines MARKDOWN\nscript:\SCRIPT tag pattern
+			run = this.replace( pattern , (str, xscript) => {
+				script = xscript;
+				return "";
+			});
+		
+		if ( script )
+			try {
+				$( script, ctx, (vmctx) => {
+					cb( vmctx , run);
+				});
+			}
+			catch (err) {
+				cb( ctx, err+"");
+			}
+		
+		else 
+			cb(ctx, run);
+	},
+		
+	function Xkeys( ctx, cb ) {  // expand js keys ${script} || ${keys}
+		cb( this.parseEMAC(ctx) );
+	},
+	
+	function Xgen( ctx, cb ) {  // expands LHS OP= RHS tags
+
+		var 
+			pattern = /(\S*) := (\S*)/g;  // defines LHS OP= RHS tag
+		
+		cb( this.replace(pattern, (str,lhs,rhs) => {
+			//Log([":=", lhs, rhs]);
+			if ( blogOp = ctx[":="] ) 
+				return blogOp(lhs,rhs,ctx);
+			else
+				return `undefined := blog`;
+		}) );
+	},
+	
+	function Xtex( cb ) {  // expands X$$ MATH $$ tags then callbacks cb( final html )
+		var 
+			key = "@tex",
+			html = this,
+			getSite = JAX.typeset,
+			fetchTeX = function ( rec, cb ) {	// callsback cb with expanded TeX tag
+				//Log("math",rec);
+				switch (rec.arg1) {
+					case "n":
+						getSite({
+							math: rec.arg2,
+							format: "TeX",  
+							//html: true,
+							mml: true
+						}, d => cb( d.mml || "" ) );
+						break;
+					case "a":
+						getSite({
+							math: rec.arg2,
+							format: "AsciiMath",
+							//html: true,
+							mml: true
+						}, d => cb( d.mml || "" ) );
+						break;
+					case "m":
+						getSite({
+							math: rec.arg2,
+							format: "MathML", 
+							//html: true,
+							mml: true
+						}, d => cb( d.mml || "" ) );
+						break;
+					case " ":
+					default:
+						getSite({
+							math: rec.arg2,
+							format: "inline-TeX",  
+							//html: true,
+							mml: true
+						}, d => cb( " " + d.mml || "" ) );
+				}
+			},
+			pattern = /(.?)\$\$([^\$]*)\$\$/g;
+			
+		html.serialize( fetchTeX, pattern, key, (html,fails) => { 
+			cb(html);
+		}); 
+	},
+	
+	function Xparms(goto, cb) {		// expands <!---parms KEY=VAL&...---> tags then callbacks cb( final input-scripted html )
+		var
+			pattern = /<!---parms ([^>]*)?--->/g;
+		
+		cb( this.replace(pattern, (str, parms) => {
+					
+			//Log(">>>Xparms", parms);
+			
+			var 
+				inputs = [],
+				keys = [];
+
+			parms.split("&").forEach( (parm) => {  // each collect parm (key=value) needs an input
+				parm.replace(/([^=]*)?=(.*)?/, (str, key, val) => {		// key=value
+					inputs.push( `${key}: <input id="parms.${key}" type="text" value="${val}" autofocus >` );
+					keys.push( '"' + key + '"' );
+					return "";
+				});
+				return "";
+			});
+
+			// this litle marvel submits all inputs to the goto service
+			return `
+<script>
+	String.prototype.tag = ${"".tag}
+	function submitForm() {
+		var parms = {};
+		[${keys}].forEach( (key) => parms[key] = document.getElementById("parms."+key).value );
+
+		window.open( "/${goto}".tag("?", parms) );
+	}
+</script>
+<form onsubmit="submitForm()">
+	${inputs.join("")}
+	<button id="parms.submit" type="submit" value="submit">submit</button>
+</form>` ;
+
+		}) );
+	},
+	
+	function Xfetch( cb ) {  // expands <!---fetch URL---> tags then callsback cb( final url-fetched html )
+		var 
+			key = "@fetch",
+			//getSite = TOTEM.getSite,
+			fetchSite = function ( rec, cb ) {  // callsback cb with expanded fetch-tag 
+				//Log(">>>>Xfetch", rec.arg1);
+				getSite( rec.arg1, null, cb );
+			},
+			pattern = /<!---fetch ([^>]*)?--->/g;
+			
+		this.serialize( fetchSite, pattern, key, (html,fails) => cb(html) );
+	},
+
+	function Xsection( cb ) { // expand "##.... header" 
+		var
+			key = "@sec",
+			fetchSection = function (rec, cb) {
+				//Log(">>>header", rec.ID, rec.arg1, rec.arg0.length-rec.arg1.length-2 );
+				cb( rec.arg1.tag( "h"+(rec.arg0.length-rec.arg1.length-2), {} ) );
+			},
+			pattern = /^\#* (.*)\n/gm;
+		
+		this.serialize( fetchSection, pattern, key, (html,fails) => cb(html) );
+	},
+	
+	function Xskin( ctx, proxy, cb ) { // return a skin via a proxy site
+
+		var 
+			url = URL.parse(proxy || ""),
+			host = proxy ? url.host.split(".")[0] : null,
+			md = this, //proxy ? this : this.replace(/\*\*Owner\*\*/g,`**Owner** (${req.client})`),
+			header = proxy 
+				? `img(src="/shares/images/${host}.jpg", width="100%", height="15%")`
+				: "p",
+			jade = ":markdown\n\t" + md.replace(/^\n*/,"").replace(/\n/g,"\n\t");
+
+		ctx.filename = DEBE.paths.jadePath;
+		
+		try {
+			cb( JADE.compile(jade, ctx) (ctx) );
+		}
+		catch (err) {
+			// Log(err);
+			cb( err+"" );
+		}
+	}
+	
+].Extend(String);
 
 /**
 @class DEBE.Unit_tests
